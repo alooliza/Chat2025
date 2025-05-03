ab12ac317f3c9490e93b6ad8adc5fbf9caf8a784
Mabel2010 <lizalike3108@gmail.com>
2025-05-03 14:41:50 +0300

    dfgh

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const pathToIndex = path.join(__dirname, 'static', 'index.html');
+const indexHtmlFile = fs.readFileSync(pathToIndex);
+
+const pathToJS= path.join(__dirname, 'static', 'script.js');
+const JSFile = fs.readFileSync(pathToJS);
+
+
+const pathToCSS= path.join(__dirname, 'static', 'style.css');
+const CSSFile = fs.readFileSync(pathToCSS);
+
+const server = http.createServer((req, res)=>{
+    //Liza ostrologiya eto ne nayka
+    switch(req.url){
+        case "/":
+            return res.end(indexHtmlFile); 
+        case "/script.js":
+            return res.end(JSFile); 
+        case "/style.css":
+            return res.end(CSSFile);
+        case "/add":
+            addMessage(res, req);
+            break;
+        default:
+            res.statusCode = 404;
+            return res.end('Error 404');
+    }
+});
+server.listen(3000);
+function addMessage(res, req){
+    let data = ""
+    req.on('data', function(chunk){
+        data += chunk;
+    })
+    req.on('end',function(){
+        console.log(data);
+    })    
+    res.writeHead(302, {Location: '/'});
+    res.end();
+}
+
